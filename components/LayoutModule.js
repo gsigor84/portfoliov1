e56b87883a50837c1c4f243e6cca6a1e8af35b39
igor silva <igorsilva@igors-Mac-mini.local>
2022-04-11 17:27:05 +0100

    first commit

--- components/LayoutModule.js
+++ components/LayoutModule.js
@@ -11,7 +11,7 @@ export default function Layout({children}) {
         <Head>
  
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" Crossorigin/>
+<link rel="preconnect" href="https://fonts.gstatic.com" />
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Lato&display=swap" />
        </Head>
     
